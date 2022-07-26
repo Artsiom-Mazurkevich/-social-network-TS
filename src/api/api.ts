dc7a422548c7339b192b3e3a92b8bd00ac5ee809
Artsiom-Mazurkevich <patrol1995@yandex.ru>
2022-07-26 22:10:36 +0300

    -

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,11 +2,18 @@ import axios from "axios";
 import {StateUsersType} from "../redux/users-reduser";
 
 
+
+
+
 const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     withCredentials: true,
     headers: {'API-KEY': 'b9bcb12e-ec21-423a-a02a-c9a8061be3c5'}
 })
+
+
+
+
 
 
 export const UsersAPI = {
